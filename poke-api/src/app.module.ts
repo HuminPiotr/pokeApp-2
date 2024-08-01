343d0908f5029b616c38081efbd7262a1bacf9c1
HuminPiotr <humin.piotr@gmail.com>
2024-08-01 23:33:26 +0200

    Connect postgres database

--- poke-api/src/app.module.ts
+++ poke-api/src/app.module.ts
@@ -3,14 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PokemonModule } from './pokemons/pokemon.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    PokemonModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: './database/my-db.sqlite3',
+      type: 'postgres',
+      url: process.env.DB_URL,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: true, // not for production [!]
     }),
     PokemonModule,
   ],
